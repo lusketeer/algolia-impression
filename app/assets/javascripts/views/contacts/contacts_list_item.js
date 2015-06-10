42ab53465256a40be3fa3fa314b91cf3c3068ae5
Luke <luke8520@gmail.com>
2015-06-10 08:03:02 -0400

    added click event to re-center the map

--- app/assets/javascripts/views/contacts/contacts_list_item.js
+++ app/assets/javascripts/views/contacts/contacts_list_item.js
@@ -1,11 +1,26 @@
 AlgoliaImpression.Views.ContactsListItem = Backbone.View.extend({
   template: JST["contacts/list_item"],
 
+  events: {
+    "click .contact": "goToAddress"
+  },
+
+  initialize: function(options) {
+    this.mapView = options.mapView;
+  },
+
   render: function() {
     var content = this.template({
       contact: this.model
     });
     this.$el.html(content);
+    this.$el.find("a.contact").data("id", this.model.get("objectID"));
     return this;
+  },
+
+  // Center map at clicked contact's location
+  goToAddress: function(event) {
+    event.preventDefault();
+    this.mapView.initMap(this.model.get("_geoloc"));
   }
 });

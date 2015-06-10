d30d97257debd01487b76a048442073887cc0812
Luke <luke8520@gmail.com>
2015-06-10 10:41:07 -0400

    added initialize function, and jquery to set title and submit button text

--- app/assets/javascripts/views/contacts/contacts_edit.js
+++ app/assets/javascripts/views/contacts/contacts_edit.js
@@ -2,11 +2,19 @@ AlgoliaImpression.Views.ContactsEdit = Backbone.View.extend({
 
   template: JST["contacts/form"],
 
+  initialize: function(options) {
+    if (options) {
+      this.mapView = options.mapView;
+    }
+  },
+
   render: function() {
     var content = this.template({
       contact: this.model
     });
     this.$el.html(content);
+    this.$("h4.modal-title").html("Edit Contact");
+    this.$("button.submit").html("Update Contact");
 
     return this;
   }

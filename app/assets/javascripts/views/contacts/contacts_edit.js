66dd1e8cfa7ce41e55f87fa7f84275634783701d
Luke <luke8520@gmail.com>
2015-06-13 00:30:07 -0700

    force navigate to root once a content is deleted

--- app/assets/javascripts/views/contacts/contacts_edit.js
+++ app/assets/javascripts/views/contacts/contacts_edit.js
@@ -67,6 +67,7 @@ AlgoliaImpression.Views.ContactsEdit = Backbone.View.extend({
     this.model.deleteContact(function() {
       $("#contact_modal").modal("hide");
       $("body").removeClass("modal-open");
+      Backbone.history.navigate("/", { trigger: true });
       console.log("it's deleted");
     });
   }
